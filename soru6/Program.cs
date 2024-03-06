831fc2259067e72954d36d998142af76b110e78e
Ahmet Yüksel <ahmetyuksel.ms@outlook.com>
2024-03-06 13:30:13 +0300

    6. ve 7. sorular çözüldü.

---
+++ soru6/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace soru6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO: 6- String metotlarını araştırınız. Her bir metot için örnek yapınız.
+
+            string ad = "Ahmet";
+            string soyad = "Yüksel";
+
+            // Contains("el") => Aranan harf ya da kelimenin olma durumunu kontrol eder. (True - False)
+            bool eVarMi = ad.Contains('e');
+            Console.WriteLine(eVarMi);
+
+            // EndsWith('z') => Metnin sonunda aranan harf ya da kelimenin olma durumunu kontrol eder. (True - False)
+            bool sondaElVarMi = soyad.EndsWith("el");
+            Console.WriteLine(sondaElVarMi);
+
+            // StartsWith('a') => Metnin başında aranan harf ya da kelimenin olma durumunu kontrol eder. (True - False)
+            bool bastaYuVarMi = soyad.StartsWith("Yu");
+            Console.WriteLine(bastaYuVarMi);
+
+            // Insert(indexNo, "abc") => Belirtilen index'ten sonra istenilen metinsel parçayı ekler.
+            soyad = soyad.Insert(3, "sel");
+            Console.WriteLine(soyad);
+
+            // Remove() => Belirtilen indexten sonrasını veya belirtilen indexten sonra belirtilen karakterde metin parçasını siler.
+            string silinecekMetin = "aspdotnetcoremvc";
+            Console.WriteLine(silinecekMetin.Remove(3, 3));
+
+            // Replace(' ', '-') => Metinde belirtlien karakterleri değiştirir.
+            string degistirilecekMetin = "Ahmet Yüksel 4A";
+            Console.WriteLine(degistirilecekMetin.Replace(' ', '-'));
+
+            // Substring(4, 2) => Belirtilen index'ten itibaren veya o indexten sonra belirtilen karakterde parça alır.
+            string alinacakParca = "İstanbul";
+            Console.WriteLine(alinacakParca.Substring(3, 2));
+
+            // ToLower() => Metindeki bütün harfleri küçük yapar.
+            Console.WriteLine(ad.ToLower());
+
+            // ToUpper => Metindeki bütün harfleri büyük yapar.
+            Console.WriteLine(ad.ToUpper());
+
+            // Split() => Kelimeler arasına ayraç olarak koyulan karakterle işlem yapar.
+            string bolunecekMetin = "İstanbul,Ankara,İzmir";
+
+            string[] bolunecekMetin2 = bolunecekMetin.Split(',');
+
+            for (int i = 0; i < bolunecekMetin2.Length; i++)
+            {
+                Console.WriteLine(bolunecekMetin2[i]);
+            }
+
+            // Trim() => Metnin başı ve sonundaki boşlukları siler.
+            string tiraslanacakMetin = "  Selam, bu trimlenecek bir metindir.     x";
+            Console.WriteLine(tiraslanacakMetin.Trim());
+
+            // TrimEnd() => Metnin sonundaki boşlukları siler.
+            string tiraslanacakMetin2 = "  Selam, bu trimlenecek bir metindir.     ";
+            Console.WriteLine(tiraslanacakMetin2.TrimEnd());
+
+
+            // TrimStart() => Metnin başındaki boşlukları siler.
+            string tiraslanacakMetin3 = "  Selam, bu trimlenecek bir metindir.     x";
+            Console.WriteLine(tiraslanacakMetin3.TrimStart());
+        }
+    }
+}
---
+++ soru7/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace soru7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO: 7- kullanıcıdan öğrenci sayısını isteyen her öğrenci için öğrenci adı,öğrenci soyadı,1.sınav notu,2.sınav notu,3.sınav notu isteyen ve daha sonra not ortalamasını hesaplayıp ekrana yazdıran algoritma yapınız. (For döngüsü kullanınız.)
+
+            int ilkSinav = 0, ikinciSinav = 0, ucuncuSinav = 0, ortalama = 0, ogrenciSayisi = 0;
+
+            while (ogrenciSayisi < 100)
+            {
+                try
+                {
+                    Console.Write("Notu girilecek öğrenci sayısını girin: ");
+                    ogrenciSayisi = byte.Parse(Console.ReadLine());
+
+                    for (int i = 0; i < ogrenciSayisi; i++)
+                    {
+                        Console.Write("\nAdınızı giriniz: ");
+                        string adi = Console.ReadLine();
+
+                        Console.Write("Soyadınızı giriniz: ");
+                        string soyadi = Console.ReadLine();
+
+                        Console.Write("\nİlk sınav notunuzu giriniz: ");
+                        ilkSinav = byte.Parse(Console.ReadLine());
+
+                        Console.Write("\nİkinci sınav notunuzu giriniz: ");
+                        ikinciSinav = byte.Parse(Console.ReadLine());
+
+                        Console.Write("\nÜçüncü sınav notunuzu giriniz: ");
+                        ucuncuSinav = byte.Parse(Console.ReadLine());
+
+                        ortalama = (ilkSinav + ikinciSinav + ucuncuSinav) / 3;
+
+                        Console.WriteLine($"\nÖğrencinin;\nAdı: {adi}\nSoyadı: {soyadi}\nSınavları Ortalaması: {ortalama}");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.Write($"\nHata:\n{ex}");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+            }
+        }
+    }
+}

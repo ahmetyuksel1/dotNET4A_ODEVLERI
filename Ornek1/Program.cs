04eaf41a4d96cc1bd333f86402c2f2b9928eb1bd
Ahmet Yüksel <ahmetyuksel.ms@outlook.com>
2024-03-19 15:57:21 +0300

    İlk sorunun örnekleri gerçekleştirildi.

---
+++ Ornek1/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Ornek1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Polymorphism: Bir nesnenin birden fazla biçime sahip olabilmesidir. Aynı isme sahip metotlar veya sınıfların farklı davranışlarda bulunabilmesidir.
+
+            Operation operation = new Operation();
+            operation.Message();
+
+            Operation project = new Project();
+            project.Message();
+
+            Operation software = new Software();
+            software.Message();
+        }
+    }
+
+    // Class
+    public class Operation
+    {
+        public virtual void Message()
+        {
+            Console.WriteLine("Operation is processing...\n");
+        }
+    }
+
+    // Subclass
+    class Project : Operation
+    {
+        public override void Message()
+        {
+            Console.WriteLine("Project is processing...\n");
+        }
+    }
+
+    // Subclass
+    class Software : Operation
+    {
+        public override void Message()
+        {
+            Console.WriteLine("Software is processing...");
+        }
+    }
+}
---
+++ Ornek2/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Ornek2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Polymorphism: Bir nesnenin birden fazla biçime sahip olabilmesidir. Aynı isme sahip metotlar veya sınıfların farklı davranışlarda bulunabilmesidir.
+
+            Quote quote = new Quote();
+            quote.Message();
+
+            Quote quote2 = new Quote2();
+            quote2.Message();
+
+            Quote quote3 = new Quote3();
+            quote3.Message();
+        }
+    }
+
+    // Class
+    public class Quote
+    {
+        public virtual void Message()
+        {
+            Console.WriteLine("Damnant quod non intelligunt.\n");
+        }
+    }
+
+    public class Quote2 : Quote
+    {
+        public override void Message()
+        {
+            Console.WriteLine("Be aware of a old man in a profession where men usually die young.\n");
+        }
+    }
+
+    public class Quote3 : Quote
+    {
+        public override void Message()
+        {
+            Console.WriteLine("Öl, söz verme. Öl, sözünden dönme.");
+        }
+    }
+}
